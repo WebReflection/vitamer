9be6df60aa6ddb84eb384d11ed2717cc749ed26d
eliot-akira <me@eliotakira.com>
2015-12-19 14:23:36 +0100

    build bundle

--- src/vitamer.js
+++ src/vitamer.js
@@ -1,1 +1,10 @@
-{}
+require('dom4');
+require('document-register-element');
+require('document-register-element/build/innerHTML');
+
+window.$ = require('query-result');
+window.Class = require('es-class');
+window.restyle = require('restyle');
+window.DOMClass = require('dom-class');
+
+require('dom-class/build/dom-class-mixins');
---
+++ src/vitamer.max.js
@@ -0,0 +1,10 @@
+require('dom4/build/dom4.max');
+require('document-register-element/build/document-register-element.max');
+require('document-register-element/build/innerHTML.max');
+
+window.$ = require('query-result/build/query-result.max');
+window.Class = require('es-class/build/es-class.npm');
+window.restyle = require('restyle/build/restyle.node');
+window.DOMClass = require('dom-class/build/dom-class.max');
+
+require('dom-class/build/dom-class-mixins');
